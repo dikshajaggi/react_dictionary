548d8bc9c1dbe6bdb3dc7776e863665a159991ab
cee-diksha <diksha2000may@gmail.com>
2024-03-29 20:09:57 +0530

    minor modifications

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Definitions from './components/Definitions/Definitions';
 import { grey } from '@material-ui/core/colors';
 
 function App() {
-  const [meanings , setmeanings] = useState([]);
+  const [meanings, setmeanings] = useState([]);
   const [word, setword] = useState("");
   const [category, setcategory] = useState("en");
   const [LightTheme, setLightTheme] = useState(false);
@@ -26,49 +26,51 @@ function App() {
     track: {},
   })(Switch);
 
-  const dictionaryApi = async() => {
-    try{
-      const data = await axios.get( `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`);
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`);
       setmeanings(data.data);
     }
-    catch(error){
+    catch (error) {
       console.log(error);
     }
   };
 
   console.log(meanings);
 
-  useEffect(() =>{
+  useEffect(() => {
     dictionaryApi();
+    // eslint-disable-next-line
   }, [word, category]);
-  
+
   return (
     <div className="App" style={{
       height: "100vh",
       backgroundColor: LightTheme ? "#fff" : "#282c34",
       color: LightTheme ? "black" : "white",
-      transition: "all 0.5s linear"}}>
-  
-  
+      transition: "all 0.5s linear"
+    }}>
+
+
       <Container maxWidth="md" style={{
-            display: "flex",
-            flexDirection: "column",
-            height: "100vh",
-            justifyContent: "space-evenly",
-          }}> 
-           <div
-            style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
-          >
-            <span>{LightTheme ? "Dark" : "Light"} Mode</span>
-            <Mode
-              checked={LightTheme}
-              onChange={() => setLightTheme(!LightTheme)}
-            />
-          </div>
-  
-          <Header category={category}  setcategory={setcategory} word={word} setword={setword} LightTheme={LightTheme}/> 
-  
-          {meanings && (<Definitions word={word} category={category} meanings={meanings} LightTheme={LightTheme}/>)}
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        justifyContent: "space-evenly",
+      }}>
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+        >
+          <span>{LightTheme ? "Dark" : "Light"} Mode</span>
+          <Mode
+            checked={LightTheme}
+            onChange={() => setLightTheme(!LightTheme)}
+          />
+        </div>
+
+        <Header category={category} setcategory={setcategory} word={word} setword={setword} setMeanings={setmeanings} LightTheme={LightTheme} />
+
+        {meanings && (<Definitions word={word} category={category} meanings={meanings} LightTheme={LightTheme} />)}
       </Container>
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,57 +1,58 @@
 import React from 'react';
 import './Header.css';
-import {createTheme,ThemeProvider, TextField, MenuItem } from '@material-ui/core';
+import { createTheme, ThemeProvider, TextField, MenuItem } from '@material-ui/core';
 import categories from '../data/category';
 
-const Header = ({category , setcategory, word , setword, setMeanings, LightTheme}) =>{
+const Header = ({ category, setcategory, word, setword, setMeanings, LightTheme }) => {
     const darkTheme = createTheme({
         palette: {
-            primary:{
+            primary: {
                 main: LightTheme ? "#000" : "#fff",
             },
             type: LightTheme ? "light" : "dark",
         },
-      });
+    });
 
-      const handlechange = (Language) =>{
+    const handlechange = (Language) => {
+        console.log("lang", Language)
         setcategory(Language);
         setword("");
         setMeanings([]);
-      }
+    }
 
     return (
-    <div className="header">
-        <span className="title">{word ? word : "Word Finder"} </span>
-        <div className="inputs">  
-            <ThemeProvider theme={darkTheme}>
-                <TextField 
-                    id="standard-basic" 
-                    label="Search a Word" 
-                    className="search" 
-                    value={word}
-                    onChange={(e)=>setword(e.target.value)}
+        <div className="header">
+            <span className="title">{word ? word : "Word Finder"} </span>
+            <div className="inputs">
+                <ThemeProvider theme={darkTheme}>
+                    <TextField
+                        id="standard-basic"
+                        label="Search a Word"
+                        className="search"
+                        value={word}
+                        onChange={(e) => setword(e.target.value)}
                     />
-                <TextField
-                    select
-                    className="select"
-                    label= "Language"
-                    value= {category}
-                    onChange= {(e)=>handlechange(e.target.value)}
-                >
+                    <TextField
+                        select
+                        className="select"
+                        label="Language"
+                        value={category}
+                        onChange={(e) => handlechange(e.target.value)}
+                    >
 
-            {
-                categories.map((option) =>(
-                    <MenuItem key= {option.label} value={option.label} >
-                        {option.value}
-                    </MenuItem>
-                ))
-            }
-            
-                 </TextField>
-            </ThemeProvider>
-            
+                        {
+                            categories.map((option) => (
+                                <MenuItem key={option.label} value={option.label} >
+                                    {option.value}
+                                </MenuItem>
+                            ))
+                        }
+
+                    </TextField>
+                </ThemeProvider>
+
+            </div>
         </div>
-    </div>
     );
 };
 
